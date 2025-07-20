147d91df2d037c4a3bbe459d7ce21239668b2b08
Yugam Akharia <yugamakharia@email.com>
2025-07-20 14:22:05 +0530

    delete & update refactoring the menu. finalized day2 features

--- inventory.py
+++ inventory.py
@@ -1,33 +1,57 @@
 a = {"name": "Maggi", "price": 12, "quantity": 1}
-l= []
-def add_items():
+inventory= []
+def add_item():
     while True:
         name = input("Enter item name: ")
         price = int(input("Enter item price: "))
         quantity = int(input("Enter item quantity: "))
 
         item = {"name": name, "price": price, "quantity": quantity}
-        l.append(item)
-
+        inventory.append(item)
+        print("Item added Succesfully! ")
         ch = input("Do you want to add another item? (y/n): ")
         if ch.lower() != 'y':
             break
 
-def readall():
+def view_items():
     print("All items: ")
-    for item in l:
+    for item in inventory:
             print(item)
+
+
+def update_item():
+    name = input("Enter item name to update: ")
+    for item in inventory:
+        if item["name"] == name:
+            new_qty = int(input("Enter new quantity: "))
+            item["quantity"] = new_qty
+            print("Quantity updated.")
+            return
+    print("Item not found! ")
+
+def delete_item():
+    name = input("Enter item name to delete: ")
+    for i, item in enumerate(inventory):
+        if item["name"] == name:
+            del inventory[i]
+            print("Item deleted.")
+            return
+    print("Item not found.")        
 
 def menu():
     while True:    
         print("Select Function:" \
         " 1) Add Item, 2) View All, 3) Exit")
         ch=int(input("Choose function (1,2 or 3): "))
-        if ch==1:
-            add_items()
-        elif ch==2:
-            readall()
-        elif ch==3:
+        if ch == "1":
+            add_item(inventory)
+        elif ch == "2":
+            view_items(inventory)
+        elif ch == "3":
+            update_item(inventory)
+        elif ch == "4":
+            delete_item(inventory)
+        elif ch=="5":
             print("Thank you for using our system!")
             break
         else:
